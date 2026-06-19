c00770cefb1c1aa26b7a0ce33e5672b599ac7a24
Jia Singla <jiasingla2006@gmail.com>
2026-06-19 22:56:14 +0530

    update db config for deployment

--- Backend/db.js
+++ Backend/db.js
@@ -1,18 +1,20 @@
 import mysql from 'mysql2'
 import 'dotenv/config'
 
-const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    database:"newdb",
-password:process.env.MYSQL_PASSWORD,
-
+const db = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 })
-db.connect((err)=>{
-    if (err) {
-        console.error(" DB Connection Failed:", err.message)
-        process.exit()
-    }
-    console.log("DB Connected Successfully")}
-)
+
+db.connect((err) => {
+  if (err) {
+    console.error("DB Connection Failed:", err.message)
+    process.exit()
+  }
+  console.log("DB Connected Successfully")
+})
+
 export default db
