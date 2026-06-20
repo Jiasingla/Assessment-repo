490740b394acb8ca267589437dd42462917bf613
Jia Singla <jiasingla2006@gmail.com>
2026-06-20 13:17:57 +0530

    fix port and add start script

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@
 import app from './index.js'
 
 
-app.listen(3000,()=>{
-    console.log("server running successfully")
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running successfully on port ${PORT}`)
 })
